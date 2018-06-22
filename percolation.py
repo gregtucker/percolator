d855514dd7ccf6dbdec56af1a8ef13f1f4e1ce25
Greg Tucker <gtucker@colorado.edu>
2018-06-21 20:12:24 -0600

    add tests; flip to (1,1) is top left

--- percolation.py
+++ percolation.py
@@ -34,47 +34,20 @@ class Percolator(object):
         -----
         Make array (n x n) + 2, with the extra two sites representing the 
         'master' points below (n*n) and above (n*n+1) the grid.
+             Create two WeightedQuickUnion objects: one will connect open cells
+        along the bottom row to the extra bottom cell. This one is used to 
+        test for percolation. The other will not connect to the extra bottom
+        cell; this one will be used to establish whether cells are full or not.
+        This strategy prevents "backwash" through the extra bottom cell.
         """
         if n <= 0:
             raise ValueError
         self.n = n
         self.cell_state = np.zeros(n*n, dtype=bool)
         self.wqu = WeightedQuickUnion(n*n+2)
+        self.wqu_connected = WeightedQuickUnion(n*n+2)
         self.extra_bottom_cell = n * n
         self.extra_top_cell = self.extra_bottom_cell + 1
-        #self._connect_extra_top_and_bottom_cells()
-
-#    def _connect_extra_top_and_bottom_cells(self):
-#        """Connect cells in bottom & top rows to extra bottom & top cell.
-#        
-#        Notes
-#        -----
-#        We introduce an extra cell 'below' the grid, and an extra one 'above',
-#        in order to easily test whether the lattice percolates: if these top
-#        and bottom cells are connected, then the whole lattice percolates.
-#
-#        Examples
-#        --------
-#        >>> p = Percolator(2)
-#        >>> p.wqu.connected(0, 4)
-#        True
-#        >>> p.wqu.connected(3, 5)
-#        True
-#        >>> p.wqu.connected(0, 2)
-#        False
-#        >>> p.wqu.connected(0, 5)
-#        False
-#        >>> p.wqu.id
-#        array([4, 4, 5, 5, 4, 5])
-#        >>> p.wqu.sz
-#        array([1, 1, 1, 1, 3, 3])
-#        """
-#        self.extra_bottom_cell = self.n ** 2
-#        self.extra_top_cell = self.extra_bottom_cell + 1
-#        offset = self.n * (self.n - 1)
-#        for i in range(self.n):
-#            self.wqu.union(self.extra_bottom_cell, i)
-#            self.wqu.union(self.extra_top_cell, i + offset)
 
     def _row_col_to_id(self, row, col):
         """Return array ID equivalent to given row, col.
@@ -95,30 +68,31 @@ class Percolator(object):
 
     def open(self, row, col):
         """Convert cell (row, col) to Open state and connect to neighbors.
-        
+
         Examples
         --------
         >>> p = Percolator(3)
-        >>> p.open(1, 1)
-        >>> p.wqu.connected(0, 9)  # now connected to extra bottom cell
+        >>> p.open(3, 1)  # lower left
+        >>> p.wqu.connected(6, 9)  # wqu not connected to extra bottom cell...
+        False
+        >>> p.wqu_connected.connected(6, 9)  # but wqu_connected is
         True
-        >>> p.wqu.connected(0, 1)  # not connected to closed neighbor to right
+        >>> p.wqu.connected(6, 7)  # not connected to closed neighbor to right
+        False
+        >>> p.open(1, 2)  # top middle
+        >>> p.wqu.connected(1, 10) # now connected to extra top cell
+        True
+        >>> p.wqu.connected(1, 4)  # not connected to neighbor below
         False
         >>> p.open(3, 2)
-        >>> p.wqu.connected(7, 10) # now connected to extra top cell
-        True
-        >>> p.wqu.connected(7, 4)  # not connected to neighbor below
-        False
-        >>> p.open(1, 2)
-        >>> p.wqu.connected(0, 1)  # now (1,1) and (1,2) are connected
+        >>> p.wqu.connected(6, 7)  # now (3,1) and (3,2) are connected
         True
         >>> p.open(2, 2)
-        >>> p.wqu.connected(4, 0)  # should connect to (1, 1) via (1, 2)
+        >>> p.wqu.connected(4, 6)  # should connect to (3, 1) via (3, 2)
         True
         >>> p.percolates()
         True
         """
-        #TODO: flip top and bottom
         if not self.is_open(row, col):
             cell = self._row_col_to_id(row, col)
             self.cell_state[cell] = True  # open it
@@ -126,22 +100,27 @@ class Percolator(object):
                 if self.is_open(row, col - 1):
                     nbr = self._row_col_to_id(row, col - 1)
                     self.wqu.union(cell, nbr)
+                    self.wqu_connected.union(cell, nbr)
             if col < self.n:  # right
                 if self.is_open(row, col + 1):
                     nbr = self._row_col_to_id(row, col + 1)
                     self.wqu.union(cell, nbr)
-            if row == self.n:  # top row: attach to extra top cell
+                    self.wqu_connected.union(cell, nbr)
+            if row == 1:  # top row: attach to extra top cell
                 self.wqu.union(self.extra_top_cell, cell)
+                self.wqu_connected.union(self.extra_top_cell, cell)
             else: # above
-                if self.is_open(row + 1, col):
-                    nbr = self._row_col_to_id(row + 1, col)
-                    self.wqu.union(cell, nbr)
-            if row == 1:  # bottom row: attach to extra bottom cell
-                self.wqu.union(self.extra_bottom_cell, cell)
-            else:  # below
                 if self.is_open(row - 1, col):
                     nbr = self._row_col_to_id(row - 1, col)
                     self.wqu.union(cell, nbr)
+                    self.wqu_connected.union(cell, nbr)
+            if row == self.n:  # bottom row: attach to extra bottom cell
+                self.wqu_connected.union(self.extra_bottom_cell, cell)
+            else:  # below
+                if self.is_open(row + 1, col):
+                    nbr = self._row_col_to_id(row + 1, col)
+                    self.wqu.union(cell, nbr)
+                    self.wqu_connected.union(cell, nbr)
 
     def is_open(self, row, col):
         """Return True if cell (row, col) is open, False otherwise."""
@@ -161,21 +140,57 @@ class Percolator(object):
         >>> p = Percolator(2)
         >>> p.is_full(2, 1)
         False
+        >>> p = Percolator(3)
+        >>> p.open(1, 1)
+        >>> p.is_full(1, 1)
+        True
+        >>> p.open(3, 1)
+        >>> p.is_full(3, 1)
+        False
+        >>> p.open(2, 1)
+        >>> p.is_full(3, 1)
+        True
+        >>> p.open(3, 3)
+        >>> p.is_full(3, 3)  # NOT full, because no back-wash
+        False
         """
-        # TODO: add more tests once open() works
         return (self.is_open(row, col)
                 and self.wqu.connected(self._row_col_to_id(row, col),
                                        self.extra_top_cell))
 
     def number_of_open_sites(self):
-        """Return number of open sites."""
-        #TODO: ADD A TEST
+        """Return number of open sites.
+        
+        Examples
+        --------
+        >>> p = Percolator(2)
+        >>> p.open(1, 1)
+        >>> p.open(2, 2)
+        >>> p.number_of_open_sites()
+        2
+        """
         return np.sum(self.cell_state)
 
     def percolates(self):
-        """Return True if lattice percolates, False otherwise."""
-        #TODO: ADD A TEST
-        return self.wqu.connected(self.extra_bottom_cell, self.extra_top_cell)
+        """Return True if lattice percolates, False otherwise.
+
+        Examples
+        --------
+        >>> p = Percolator(3)
+        >>> p.open(1, 1)
+        >>> p.open(2, 2)
+        >>> p.open(3, 3)
+        >>> p.percolates()
+        False
+        >>> p.open(1, 2)
+        >>> p.percolates()
+        False
+        >>> p.open(2, 3)
+        >>> p.percolates()
+        True
+        """
+        return self.wqu_connected.connected(self.extra_bottom_cell,
+                                            self.extra_top_cell)
 
     
 def test_perc():
